ab767e34875b6d3cff29ceaccc056415f9bdb3a4
hschang <hschang@iastate.edu>
2019-08-24 22:48:03 -0500

    Updates

---
+++ precache-manifest.553f55004d324ff23d6b5cf7e0a8eb99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1603aa7720800f70c7c08cee5ac729d2",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "1a6b69e0b52c114bb5f9",
+    "url": "/react-tetris/static/css/main.6b7ee09b.chunk.css"
+  },
+  {
+    "revision": "a3beef3611365cc84d6f",
+    "url": "/react-tetris/static/js/2.0be44faf.chunk.js"
+  },
+  {
+    "revision": "1a6b69e0b52c114bb5f9",
+    "url": "/react-tetris/static/js/main.f6c8b6d0.chunk.js"
+  },
+  {
+    "revision": "1c7f23320d41425a8f76",
+    "url": "/react-tetris/static/js/runtime~main.624370ec.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
